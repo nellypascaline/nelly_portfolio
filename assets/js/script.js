f5946950aa583ac796646fab1b090b5f88cec2f2
JUSTAS AKON. <yusinyu002@gmail.com>
2025-12-15 02:57:13 -1100

    add markup files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,46 @@
+//implimentation of mobile_menu//
 const mobileMenu = document.getElementById('mobile_menu');
 mobileMenu.addEventListener('click', () => {
   const desktopMenu = document.getElementById('desktop_menu');
   mobileMenu.classList.toggle('active');
   desktopMenu.classList.toggle('active');
 });
+//an array of objects to store project cards//
+const projects = [
+{
+  image:'#',
+  title:'Multi-Post Stories Gain+Glory',
+  technologies:['Ruby on Rails','CSS','JavaScript','HTML'],
+  button:'#'
+},
+{
+  image:'#',
+  title:'Multi-Post Stories Gain+Glory',
+  technologies:['Ruby on Rails','CSS','JavaScript','HTML'],
+  button:'#'
+},
+{
+  image:'#',
+  title:'Multi-Post Stories Gain+Glory',
+  technologies:['Ruby on Rails','CSS','JavaScript','HTML'],
+  button:'#'
+},
+{
+  image:'#',
+  title:'Multi-Post Stories Gain+Glory',
+  technologies:['Ruby on Rails','CSS','JavaScript','HTML'],
+  button:'#'
+},
+{
+  image:'#',
+  title:'Multi-Post Stories Gain+Glory',
+  technologies:['Ruby on Rails','CSS','JavaScript','HTML'],
+  button:'#'
+},
+{
+  image:'#',
+  title:'Multi-Post Stories Gain+Glory',
+  technologies:['Ruby on Rails','CSS','JavaScript','HTML'],
+  button:'#'
+},
+];
